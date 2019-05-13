222723323281c594ab4d659bf7962d2382474496
yafiabiyyu <luckyveri3@gmail.com>
2019-05-13 10:19:44 +0700

    Add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FriendComponent } from './friend/friend.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ContactUsComponent,
+    FriendComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
